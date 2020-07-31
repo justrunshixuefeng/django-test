5f65192ab0063bba187e058d05bc2fd23844caa6
shixf <shixuefeng@fuyichen.cn>
2020-07-31 11:03:11 +0800

    更改数据库的ip地址

--- django_test/test1/test1/settings.py
+++ django_test/test1/test1/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'NAME': 'runyi',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
+        'HOST': '117.132.193.165',
         'PORT': '3306',
     }
 }
